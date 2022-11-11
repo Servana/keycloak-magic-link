8ebe2fd9a683e01091fe927944cca2667f2c285c
DhruvJ225 <dhruv@servanamanaged.com>
2022-11-11 12:37:17 +0530

    Change token validity to 15 mins

--- src/main/java/io/phasetwo/keycloak/magic/MagicLink.java
+++ src/main/java/io/phasetwo/keycloak/magic/MagicLink.java
@@ -88,7 +88,7 @@ public class MagicLink {
   public static MagicLinkActionToken createActionToken(
       UserModel user, String clientId, String redirectUri, OptionalInt validity) {
     // build the action token
-    int validityInSecs = validity.orElse(60 * 60 * 24); // 1 day
+    int validityInSecs = validity.orElse(60 * 15); // 15 minutes
     int absoluteExpirationInSecs = Time.currentTime() + validityInSecs;
     MagicLinkActionToken token =
         new MagicLinkActionToken(user.getId(), absoluteExpirationInSecs, clientId, redirectUri);
